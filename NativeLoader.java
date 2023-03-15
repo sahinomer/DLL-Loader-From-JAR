9e82568f04b8da807e1b9f64015e853ae33bb05f
Ömer Şahin <sahin.omer@outlook.com>
2023-03-15 21:29:03 +0300

    Update NativeLoader.java

--- NativeLoader.java
+++ NativeLoader.java
@@ -11,23 +11,23 @@ public class NativeLoader {
 	public static void loadNativeLibrary(String name) throws IOException{
 		
 		byte[] buffer = new byte[1024];
-	    int read = -1;
-		
+		int read = -1;
+
 		File temp = File.createTempFile(name, ".dll");
 		temp.deleteOnExit();
-	
+
 		InputStream inStream = NativeLoader.class.getResourceAsStream("/lib/" + name + ".dll");
-	
-	    FileOutputStream fOutStream = new FileOutputStream(temp);
 
-	    while((read = inStream.read(buffer)) != -1) {
-	        fOutStream.write(buffer, 0, read);
-	    }
-	    
-	    fOutStream.close();
-	    inStream.close();
+		FileOutputStream fOutStream = new FileOutputStream(temp);
 
-	    System.load(temp.getAbsolutePath());
+		while((read = inStream.read(buffer)) != -1) {
+			fOutStream.write(buffer, 0, read);
+		}
+
+		fOutStream.close();
+		inStream.close();
+
+		System.load(temp.getAbsolutePath());
 
 	}
 	
